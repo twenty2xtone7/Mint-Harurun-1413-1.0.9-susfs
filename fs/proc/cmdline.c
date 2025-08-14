c96f3b7f68c7ebd178ee1339e27f47460a63b660
MD. Jadly Bin Salam Shovon <shovon129131@gmail.com>
2025-08-14 13:34:15 +0600

    [BACKPORT] Implemented SUSFS v1.5.9
    There is a few change in here:
    - sus_mount functionality still remain in v1.5.5 as backporting it to the latest version will result a mount detection leak in some apps/detectors
    
    Thank you @simonpunk for the help of sus_path backport

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -12,10 +12,19 @@ enum {
 
 static char new_command_line[COMMAND_LINE_SIZE];
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
+#endif
+
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_puts(m, new_command_line);
-	seq_putc(m, '\n');
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+	if (!susfs_spoof_cmdline_or_bootconfig(m)) {
+		seq_putc(m, '\n');
+		return 0;
+	}
+#endif
+	seq_printf(m, "%s\n", saved_command_line);
 	return 0;
 }
 
