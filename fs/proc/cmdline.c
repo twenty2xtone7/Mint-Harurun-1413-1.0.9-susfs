7ea6e56d64185c03c165724c7c0f39029978c4c5
MD. Jadly Bin Salam Shovon <shovon129131@gmail.com>
2025-08-15 09:15:29 +0600

    Merge pull request #4 from MrShockWAVEog/susfs-1.5.9
    Updated SUSFS to Latest 1.5.9

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -16,6 +16,10 @@ static char new_command_line[COMMAND_LINE_SIZE];
 extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
 #endif
 
+#ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
+extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
+#endif
+
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
 #ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
